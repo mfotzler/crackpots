16f7eaa0ad449c8eccb9a918b75d4210759dc526
Matthew Fotzler <mfotzler@gmail.com>
2023-02-22 15:29:25 -0600

    Add ability to push pots off the edge of the building

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    [SerializeField] private float movementSpeed = 6f;
+    [SerializeField] private float movementSpeed = 8f;
+    [SerializeField] private float potPushForce = 100f;
+    [SerializeField] private float potPushProximity = 0.5f;
     void Update()
     {
         if (!IsOwner)
@@ -24,9 +27,25 @@ public class PlayerNetwork : NetworkBehaviour
         transform.position += movementVector * (movementSpeed * Time.deltaTime);
     }
 
-    [ServerRpc]
-    public void PushPotServerRpc()
+    [ServerRpc(RequireOwnership = false)]
+    public void PushPotServerRpc(ServerRpcParams serverRpcParams = default)
     {
-
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        if (NetworkManager.ConnectedClients.ContainsKey(clientId))
+        {
+            var client = NetworkManager.ConnectedClients[clientId];
+            var playerPosition = client.PlayerObject.transform.position;
+            var pots = ReferenceManager.Pots;
+            var transforms = pots.GetComponentsInChildren<Transform>();
+            foreach (Transform potTransform in transforms)
+            {
+                var distance = Mathf.Abs(potTransform.position.x - playerPosition.x);
+                Debug.Log(distance);
+                if (distance < potPushProximity)
+                {
+                    potTransform.GetComponent<Rigidbody>().AddForce(Vector3.back * potPushForce);
+                }
+            }
+        }
     }
 }
